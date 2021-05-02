45533d6102e5a8774b823994c6dc23e1371ad1b7
ezeilo-su <ezeilo.su@gmail.com>
2021-05-02 13:53:18 +0100

    Add api remote url.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -6,9 +6,12 @@ export const REQ_HEADER = {
   },
 };
 
-export const COURSE_URL = 'http://localhost:3001/api/v1/courses';
-export const COURSES_INDEX_URL = 'http://localhost:3001/api/v1/courses';
-export const LOGIN_URL = 'http://127.0.0.1:3001/api//v1/login';
-export const SIGNUP_URL = 'http://127.0.0.1:3001/api//v1/signup';
-// const SIGNUP_URL = 'https://findmycourse.herokuapp.com/api//v1/signup';
-// const LOGIN_URL = 'https://findmycourse.herokuapp.com/api//v1/login';
+// export const COURSE_URL = 'http://localhost:3001/api/v1/courses';
+// export const COURSES_INDEX_URL = 'http://localhost:3001/api/v1/courses';
+// export const LOGIN_URL = 'http://127.0.0.1:3001/api//v1/login';
+// export const SIGNUP_URL = 'http://127.0.0.1:3001/api//v1/signup';
+
+export const COURSE_URL = ' https://findmycourse.herokuapp.com/api/v1/courses';
+export const COURSES_INDEX_URL = ' https://findmycourse.herokuapp.com/api/v1/courses';
+export const LOGIN_URL = ' https://findmycourse.herokuapp.com/api//v1/login';
+export const SIGNUP_URL = ' https://findmycourse.herokuapp.com/api//v1/signup';
